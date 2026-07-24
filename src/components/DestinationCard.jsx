1ad320d8cc1b86d8480420d2f551305158c5d3fc
rrajpaul <rrajpaul@gmail.com>
2026-07-24 18:15:29 -0400

    Update DestinationCard to lazy load

--- src/components/DestinationCard.jsx
+++ src/components/DestinationCard.jsx
@@ -46,6 +46,8 @@ const DestinationCard = ({ data, destinationKey, className }) => {
             <img 
               src={imageSrc} 
               alt={imageAlt}
+              loading="lazy"
+              decoding="async"
               className="w-full h-full object-cover transition-transform duration-700 group-hover:scale-110"
             />
           </div>
