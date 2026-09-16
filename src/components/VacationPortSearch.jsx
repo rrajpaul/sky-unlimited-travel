655039fe738c8fb64c52f889a24692f4c26a433b
rrajpaul <rrajpaul@gmail.com>
2026-09-16 16:59:27 -0400

    Change height to 500px for VacationPort search

--- src/components/VacationPortSearch.jsx
+++ src/components/VacationPortSearch.jsx
@@ -40,7 +40,7 @@ export default function VacationPortSearch({ page = "/SharedPage/DefaultSearch" 
         title="VacationPort Travel Search"
         style={{
           width: "100%",
-          minHeight: "800px",
+          minHeight: "500px",
           border: "none",
           borderRadius: "8px",
         }}
